dbcd5e2c787ec731c716bca95f0f2dae415435d0
Filip Hristovski <filiphristovski@Filips-Mac-mini.local>
2018-10-08 12:44:04 +0200

    Merged INS_10 with INS_41

--- app/src/main/java/com/interworks/inspektar/home/HomeActivity.java
+++ app/src/main/java/com/interworks/inspektar/home/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.interworks.inspektar.home;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.internal.BottomNavigationMenuView;
@@ -17,30 +19,42 @@ import android.util.TypedValue;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.interworks.inspektar.MainActivity;
 import com.interworks.inspektar.R;
 import com.interworks.inspektar.base.BaseActivity;
+import com.interworks.inspektar.base.BaseViewModel;
+import com.interworks.inspektar.camera.view.CameraActivity;
+import com.interworks.inspektar.databinding.ActivityHomeBinding;
 
 import java.util.ArrayList;
 
-public class HomeActivity extends BaseActivity{
+import butterknife.BindView;
+import butterknife.OnClick;
+
+public class HomeActivity extends AppCompatActivity{
 
     private ArrayList<VideoListTestData> videoList;
     private Toolbar mTopToolbar;
+
+    ActivityHomeBinding binding;
 
     final Fragment homeFragment = new HomeFragment();
 
     final FragmentManager fm = getSupportFragmentManager();
     Fragment active = homeFragment;
 
-    FloatingActionButton fab;
+    @BindView(R.id.camera_button)
+    Button camera_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_home);
+        binding =  binding = DataBindingUtil.setContentView(this, R.layout.activity_home);
 
         mTopToolbar = findViewById(R.id.toolbarTop);
         setSupportActionBar(mTopToolbar);
@@ -54,10 +68,21 @@ public class HomeActivity extends BaseActivity{
 
 
         BottomNavigationMenuView menuView = (BottomNavigationMenuView) bottomNavigationView.getChildAt(0);
+
+        binding.cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CameraActivity.startActivity(HomeActivity.this);
+            }
+        });
     }
 
-    @Override
+    /*@Override
     protected int getContentView() {
         return R.layout.activity_home;
+    }*/
+
+    public static class ClickHandlers{
+
     }
 }
